c001133411b7a4b582c5df72ed7f8c3c29b35dea
nadeem590 <nadeem@leewayhertz.com>
2021-08-05 23:13:35 +0530

    responsive added

---
+++ src/modules/header/header.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { Column, Row } from "simple-flexbox";
+
+
+export default function HeaderMain(){
+
+    return(
+        <div>
+             <Row className="row-1">
+                <Column>
+                <Row>
+                <Column>
+                <img className="header-logo" src={require("../../assets/styles/images/xdc_logo.svg")} ></img>
+                </Column>
+                <Column className="xdc">
+                <p>XDC</p>
+                </Column>
+                </Row>
+                </Column>
+                <Column>
+                <div className="connect-wallet">
+               <div className="circle"></div>
+           
+              <p>connect wallet</p> 
+           </div>
+                </Column>
+
+
+            </Row>
+        </div>
+
+    )
+}
