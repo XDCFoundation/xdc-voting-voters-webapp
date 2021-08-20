60d5cd7d392ba4ca91ca113299df655c2bba1e3e
deepali <deepali@leewayhertz.com>
2021-08-20 11:31:58 +0530

    responsiveness_done

--- src/modules/Dashboard/viewProposal.js
+++ src/modules/Dashboard/viewProposal.js
@@ -47,9 +47,10 @@ const useStyles = makeStyles((theme) => ({
     alignItems: "center",
     height: "53px",
     background: "#FFFFFF 0% 0% no-repeat padding-box",
-    border: "1px solid #E3E7EB",
+    // border: "1px solid #E3E7EB",
     borderRadius: "6px",
     opacity: "1",
+    justifyContent: "space-between",
   },
 
   input: {
@@ -64,6 +65,7 @@ const useStyles = makeStyles((theme) => ({
     borderRadius: "4px",
     opacity: 1,
     fontSize: "11px",
+    marginLeft: "10px",
   },
 
   line: {
@@ -71,9 +73,23 @@ const useStyles = makeStyles((theme) => ({
     marginTop: "0px",
     marginBottom: "0px",
   },
+  mobilemedia: {
+    display: "flex",
+    "@media (min-width: 400px) and (max-width: 767px)": {
+      display: "block",
+      flexDirection: "Column",
+    },
+  },
 }));
-const Container = styled.div``;
-const SecondContainer = styled.div``;
+const Container = styled.div`
+  display: flex;
+`;
+const SecondContainer = styled.div`
+  display: flex;
+  @media (min-width: 300px) and (max-width: 1038px) {
+    display: none;
+  }
+`;
 
 export default function ViewAllProposal() {
   const classes = useStyles();
@@ -138,9 +154,6 @@ export default function ViewAllProposal() {
                 <Row>
                   <Status>Status :</Status>
                   <Passed>Passed</Passed>
-                  <Positiondiv>
-                    <Numberofvotes>145&nbsp;votes</Numberofvotes>
-                  </Positiondiv>
                 </Row>
                 <Row>
                   <Mobileresponsive>
@@ -156,7 +169,7 @@ export default function ViewAllProposal() {
               <Column>
                 <Row>
                   <Posted>
-                    Posted&nbsp;on &nbsp;24&nbsp; June&nbsp; 2021&nbsp;
+                    Posted&nbsp;on &nbsp;24&nbsp; June&nbsp;2021&nbsp;
                   </Posted>
 
                   <TimeRemainingdiv>
@@ -165,17 +178,19 @@ export default function ViewAllProposal() {
                   </TimeRemainingdiv>
                 </Row>
                 <Row>
-                  <Content>
-                    {" "}
-                    Adding &nbsp;more &nbsp; features &nbsp; to &nbsp;XDC &nbsp;
-                    Explorer 2.0
-                  </Content>
-                  <Positiondivline>
-                    <Barline>
-                      <Greenline></Greenline>
-                      <Redline></Redline>
-                    </Barline>{" "}
-                  </Positiondivline>
+                  <div className={classes.mobilemedia}>
+                    <Content>
+                      {" "}
+                      Adding &nbsp;more &nbsp; features &nbsp; to &nbsp;XDC
+                      &nbsp; Explorer 2.0
+                    </Content>
+                    <Positiondivline>
+                      <Barline>
+                        <Greenline></Greenline>
+                        <Redline></Redline>
+                      </Barline>{" "}
+                    </Positiondivline>
+                  </div>
                 </Row>
                 <Row>
                   <Status>Status :</Status>
@@ -205,13 +220,15 @@ export default function ViewAllProposal() {
                   </TimeRemainingdiv>
                 </Row>
                 <Row>
-                  <Content>Relaunch of XinFin.org website</Content>
-                  <Positiondivline>
-                    <Barline>
-                      <Greenline></Greenline>
-                      <Redline></Redline>
-                    </Barline>{" "}
-                  </Positiondivline>
+                  <div className={classes.mobilemedia}>
+                    <Content>Relaunch of XinFin.org website</Content>
+                    <Positiondivline>
+                      <Barline>
+                        <Greenline></Greenline>
+                        <Redline></Redline>
+                      </Barline>{" "}
+                    </Positiondivline>
+                  </div>
                 </Row>
                 <Row>
                   <Status>Status :</Status>
@@ -242,13 +259,15 @@ export default function ViewAllProposal() {
                   </TimeRemainingdiv>
                 </Row>
                 <Row>
-                  <Content>Launching NFT Marketplace to of XDC</Content>
-                  <Positiondivline>
-                    <Barline>
-                      <Greenline></Greenline>
-                      <Redline></Redline>
-                    </Barline>{" "}
-                  </Positiondivline>
+                  <div className={classes.mobilemedia}>
+                    <Content>Launching NFT Marketplace to of XDC</Content>
+                    <Positiondivline>
+                      <Barline>
+                        <Greenline></Greenline>
+                        <Redline></Redline>
+                      </Barline>{" "}
+                    </Positiondivline>
+                  </div>
                 </Row>
                 <Row>
                   <Status>Status :</Status>
@@ -278,13 +297,15 @@ export default function ViewAllProposal() {
                   </TimeRemainingdiv>
                 </Row>
                 <Row>
-                  <Content>Partnership with Circle Stable Coin</Content>
-                  <Positiondivline>
-                    <Barline>
-                      <Greenline></Greenline>
-                      <Redline></Redline>
-                    </Barline>{" "}
-                  </Positiondivline>
+                  <div className={classes.mobilemedia}>
+                    <Content>Partnership with Circle Stable Coin</Content>
+                    <Positiondivline>
+                      <Barline>
+                        <Greenline></Greenline>
+                        <Redline></Redline>
+                      </Barline>{" "}
+                    </Positiondivline>
+                  </div>
                 </Row>
                 <Row>
                   <Status>Status :</Status>
@@ -314,13 +335,17 @@ export default function ViewAllProposal() {
                   </TimeRemainingdiv>
                 </Row>
                 <Row>
-                  <Content>XDC-ABC Bootstrapping Partnership Proposal</Content>
-                  <Positiondivline>
-                    <Barline>
-                      <Greenline></Greenline>
-                      <Redline></Redline>
-                    </Barline>{" "}
-                  </Positiondivline>
+                  <div className={classes.mobilemedia}>
+                    <Content>
+                      XDC-ABC Bootstrapping Partnership Proposal
+                    </Content>
+                    <Positiondivline>
+                      <Barline>
+                        <Greenline></Greenline>
+                        <Redline></Redline>
+                      </Barline>{" "}
+                    </Positiondivline>
+                  </div>
                 </Row>
                 <Row>
                   <Status>Status :</Status>
@@ -350,13 +375,17 @@ export default function ViewAllProposal() {
                   </TimeRemainingdiv>
                 </Row>
                 <Row>
-                  <Content>XDC-ABC Bootstrapping Partnership Proposal</Content>
-                  <Positiondivline>
-                    <Barline>
-                      <Greenline></Greenline>
-                      <Redline></Redline>
-                    </Barline>{" "}
-                  </Positiondivline>
+                  <div className={classes.mobilemedia}>
+                    <Content>
+                      XDC-ABC Bootstrapping Partnership Proposal
+                    </Content>
+                    <Positiondivline>
+                      <Barline>
+                        <Greenline></Greenline>
+                        <Redline></Redline>
+                      </Barline>{" "}
+                    </Positiondivline>
+                  </div>
                 </Row>
                 <Row>
                   <Status>Status :</Status>
@@ -386,13 +415,17 @@ export default function ViewAllProposal() {
                   </TimeRemainingdiv>
                 </Row>
                 <Row>
-                  <Content>XDC-ABC Bootstrapping Partnership Proposal</Content>
-                  <Positiondivline>
-                    <Barline>
-                      <Greenline></Greenline>
-                      <Redline></Redline>
-                    </Barline>{" "}
-                  </Positiondivline>
+                  <div className={classes.mobilemedia}>
+                    <Content>
+                      XDC-ABC Bootstrapping Partnership Proposal
+                    </Content>
+                    <Positiondivline>
+                      <Barline>
+                        <Greenline></Greenline>
+                        <Redline></Redline>
+                      </Barline>{" "}
+                    </Positiondivline>
+                  </div>
                 </Row>
                 <Row>
                   <Status>Status :</Status>
@@ -422,13 +455,17 @@ export default function ViewAllProposal() {
                   </TimeRemainingdiv>
                 </Row>
                 <Row>
-                  <Content>XDC-ABC Bootstrapping Partnership Proposal</Content>
-                  <Positiondivline>
-                    <Barline>
-                      <Greenline></Greenline>
-                      <Redline></Redline>
-                    </Barline>{" "}
-                  </Positiondivline>
+                  <div className={classes.mobilemedia}>
+                    <Content>
+                      XDC-ABC Bootstrapping Partnership Proposal
+                    </Content>
+                    <Positiondivline>
+                      <Barline>
+                        <Greenline></Greenline>
+                        <Redline></Redline>
+                      </Barline>{" "}
+                    </Positiondivline>
+                  </div>
                 </Row>
                 <Row>
                   <Status>Status :</Status>
@@ -447,36 +484,66 @@ export default function ViewAllProposal() {
               <hr className={classes.line} />
             </div>
           </Div>
-          <div>
-            <Show>Show</Show>
-            <Block>
-              <Span>10</Span>
-            </Block>
+          <Pagingdiv>
+            <Container>
+              <Show>Show &nbsp;</Show>
+              <Block>
+                <Span>10 &nbsp;</Span>
+              </Block>
 
-            <Properties>Properties</Properties>
-          </div>
+              <Properties>Properties&nbsp;</Properties>
+            </Container>
+            <SecondContainer>
+              <Block>
+                <Span>1&nbsp;</Span>
+              </Block>
+              <Block>
+                <Span>2&nbsp;</Span>
+              </Block>
+              <Block>
+                <Span>Next&nbsp;</Span>
+              </Block>
+            </SecondContainer>
+          </Pagingdiv>
         </Grid>
       </div>
     </div>
   );
 }
+const Pagingdiv = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 const Span = styled.span`
   text-align: left;
-  font: normal normal normal 14px/17px Inter;
+  font: normal normal normal 13px/17px Inter;
+  letter-spacing: 0px;
+  color: #3b3b3b;
+  opacity: 1;
+  display: flex;
+`;
+const Show = styled.span`
+  display: flex;
+  text-align: left;
+  font: normal normal normal 13px/17px Inter;
   letter-spacing: 0px;
   color: #3b3b3b;
   opacity: 1;
 `;
-const Show = styled.span``;
-const Block = styled.span`
-  color: var(--unnamed-color-2149b9);
-  text-align: left;
-  font: normal normal medium 14px/17px Inter;
-  letter-spacing: 0px;
-  color: #2149b9;
+const Block = styled.div`
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 1px solid #e3e7eb;
+  border-radius: 4px;
   opacity: 1;
 `;
-const Properties = styled.span``;
+const Properties = styled.span`
+  display: flex;
+  text-align: left;
+  font: normal normal normal 13px/17px Inter;
+  letter-spacing: 0px;
+  color: #3b3b3b;
+  opacity: 1;
+`;
 
 const TimeRemainingdiv = styled.div`
   position: absolute;
@@ -507,10 +574,12 @@ const Greenline = styled.div`
 const Redline = styled.div`
   background-color: #f43d3d;
   width: 60px;
-
   height: 3px;
 `;
-const SelectBox = styled.div``;
+const SelectBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
 const Failed = styled.span`
   text-align: left;
   font: normal normal normal 15px/19px Inter;
@@ -520,7 +589,12 @@ const Failed = styled.span`
   padding-top: 6px;
   color: #f43d3d;
 `;
-const Inputdiv = styled.div``;
+const Inputdiv = styled.div`
+  display: flex;
+  @media (min-width: 300px) and (max-width: 1038px) {
+    display: none;
+  }
+`;
 const Positiondiv = styled.div`
   position: absolute;
   right: 24%;
@@ -618,6 +692,9 @@ const Passed = styled.span`
 `;
 const Numberofvotes = styled.span`
   font: normal normal 600 14px/22px Inter;
+  @media (min-width: 400px) and (max-width: 767px) {
+    display: none;
+  }
 `;
 const Heading = styled.span`
   white-space: nowrap;
