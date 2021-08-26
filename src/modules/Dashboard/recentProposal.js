9a8819dc29d9e7506c3253c38f4ae94429cfca51
nadeem590 <nadeem@leewayhertz.com>
2021-08-26 21:52:45 +0530

    Mobile responsive done

--- src/modules/Dashboard/recentProposal.js
+++ src/modules/Dashboard/recentProposal.js
@@ -160,16 +160,19 @@ export default function RecentProposal() {
                 <div>
                     <Row className="box-all">
                         <Column className="know-about-xdc">
+                        <img className="external-image" src={require("../../assets/styles/images/External-Link.svg")} ></img>
                         <Row><img className="time-inactive" src={require("../../assets/styles/images/Group 30.svg")} ></img></Row>
                         <Row className="know-about-xdc-heading">Know about XDC Governance</Row>
                         <Row className="know-about-xdc-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit quisque venenatis.</Row>
                         </Column>
                         <Column className="xdc-forum">
+                        <img className="external-image" src={require("../../assets/styles/images/External-Link.svg")} ></img>
                         <Row><img className="time-inactive" src={require("../../assets/styles/images/Group 32.svg")} ></img></Row>
                         <Row className="know-about-xdc-heading">XDC Forum</Row>
                         <Row className="know-about-xdc-para">Mauris neque urna, sodales at molestie ut, pulvinar eu lorem.</Row>
                         </Column>
                         <Column className="community-tools">
+                        <img className="external-image" src={require("../../assets/styles/images/External-Link.svg")} ></img>
                         <Row><img className="time-inactive" src={require("../../assets/styles/images/Community-Tools.svg")} ></img></Row>
                         <Row className="know-about-xdc-heading">Community Tools</Row>
                         <Row className="know-about-xdc-para">Cras porttitor iaculis sapien eu fringilla Cras in ligula egestas ipsum quis urna.</Row>
