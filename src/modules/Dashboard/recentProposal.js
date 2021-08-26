a6095e0a21236d7c797a5049aea899a7c1907ae3
nadeem590 <nadeem@leewayhertz.com>
2021-08-26 18:54:04 +0530

    Desktp responsive done

--- src/modules/Dashboard/recentProposal.js
+++ src/modules/Dashboard/recentProposal.js
@@ -9,7 +9,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { history } from "../../managers/history";
-
+import divBlockComponent from './divComponent';
 export default function RecentProposal() {
     React.useEffect(() => {
         let address = [
@@ -151,8 +151,32 @@ export default function RecentProposal() {
                 </Grid>
             </Grid>
             {/* </div> */}
-
+           
+           
+           
             <div style={{ paddingTop: "10%" }}></div>
+            {/* !+++++++++++++++++++++! */}
+            <div className="blockdiv" style={{backgroundColor:"white"}}>
+                <div>
+                    <Row className="box-all">
+                        <Column className="know-about-xdc">
+                        <Row><img className="time-inactive" src={require("../../assets/styles/images/Group 30.svg")} ></img></Row>
+                        <Row className="know-about-xdc-heading">Know about XDC Governance</Row>
+                        <Row className="know-about-xdc-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit quisque venenatis.</Row>
+                        </Column>
+                        <Column className="xdc-forum">
+                        <Row><img className="time-inactive" src={require("../../assets/styles/images/Group 32.svg")} ></img></Row>
+                        <Row className="know-about-xdc-heading">XDC Forum</Row>
+                        <Row className="know-about-xdc-para">Mauris neque urna, sodales at molestie ut, pulvinar eu lorem.</Row>
+                        </Column>
+                        <Column className="community-tools">
+                        <Row><img className="time-inactive" src={require("../../assets/styles/images/Community-Tools.svg")} ></img></Row>
+                        <Row className="know-about-xdc-heading">Community Tools</Row>
+                        <Row className="know-about-xdc-para">Cras porttitor iaculis sapien eu fringilla Cras in ligula egestas ipsum quis urna.</Row>
+                        </Column>
+                    </Row>
+                </div>
+            </div>
         </div>
     )
 }
