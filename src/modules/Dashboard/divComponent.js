97a045c917486ceb8be23d84214ab3577978174a
nadeem590 <nadeem@leewayhertz.com>
2021-08-26 18:54:04 +0530

    Desktp responsive done

---
+++ src/modules/Dashboard/divComponent.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { Column, Row } from "simple-flexbox";
+import "../../assets/styles/custom.css";
+import TableCell from '@material-ui/core/TableCell';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { history } from "../../managers/history";
+
+export default function divBlockComponent() {
+    return(
+   <div>
+          
+          <div> HHHHHHHHHHHHHHHHHHHHHH</div>
+
+            {/* <div style={{ paddingTop: "10%" }}></div> */}
+        </div>
+    )
+}
