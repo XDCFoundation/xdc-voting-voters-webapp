92c82f22b88757edab1fcee80921d6a867768439
nadeem590 <nadeem@leewayhertz.com>
2021-08-27 15:18:21 +0530

    ui updated

--- src/modules/Dashboard/divComponent.js
+++ src/modules/Dashboard/divComponent.js
@@ -10,13 +10,47 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import { history } from "../../managers/history";
 
-export default function divBlockComponent() {
-    return(
-   <div>
-          
-          <div> HHHHHHHHHHHHHHHHHHHHHH</div>
+export default function DivBlocksComponent() {
+    
 
-            {/* <div style={{ paddingTop: "10%" }}></div> */}
+
+    return (
+        <div>
+           
+            <div style={{ paddingTop: "10%" }}></div>
+            {/* !+++++++++++++++++++++! */}
+            <div className="blockdiv" style={{backgroundColor:"white"}}>
+                <div>
+                    <Row className="box-all">
+                        <Row className="box-all1">
+                           
+                        <Row className="mobile-box-block">
+                        <Column className="know-about-xdc">
+                        <img className="external-image" src={require("../../assets/styles/images/External-Link.svg")} ></img>
+                        <Row><img className="time-inactive" src={require("../../assets/styles/images/Group 30.svg")} ></img></Row>
+                        <Row className="know-about-xdc-heading">Know about XDC Governance</Row>
+                        <Row className="know-about-xdc-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit quisque venenatis.</Row>
+                        </Column>
+                        <Column className="xdc-forum">
+                        <img className="external-image" src={require("../../assets/styles/images/External-Link.svg")} ></img>
+                        <Row><img className="time-inactive" src={require("../../assets/styles/images/Group 32.svg")} ></img></Row>
+                        <Row className="know-about-xdc-heading">XDC Forum</Row>
+                        <Row className="know-about-xdc-para">Mauris neque urna, sodales at molestie ut, pulvinar eu lorem.</Row>
+                        </Column>
+                        </Row>
+                       
+                        <Row>
+                        <Column className="community-tools">
+                        <img className="external-image" src={require("../../assets/styles/images/External-Link.svg")} ></img>
+                        <Row><img className="time-inactive" src={require("../../assets/styles/images/Community-Tools.svg")} ></img></Row>
+                        <Row className="know-about-xdc-heading">Community Tools</Row>
+                        <Row className="know-about-xdc-para">Cras porttitor iaculis sapien eu fringilla Cras in ligula egestas ipsum quis urna.</Row>
+                        </Column>
+                        </Row>
+                        </Row>
+                    </Row>
+                </div>
+            </div>
         </div>
     )
 }
