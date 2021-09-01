69adbd1614bc6e6301453cb63250e34dde9d19a1
deepali <deepali@leewayhertz.com>
2021-09-01 12:44:48 +0530

    Merge branch 'development' of https://bitbucket.org/leewayhertz/xinfin-voting-voters-webapp into xvd-99

--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,36 @@
-import React from 'react';
-import {Router, Route} from 'react-router-dom';
-import {Redirect, Switch} from "react-router";
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {connect} from "react-redux";
-import {Login, SignUp} from "./modules";
-import Header from './modules/header/headerComponent';
-import {history} from "./managers/history";
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import { Redirect, Switch } from "react-router";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
+import { connect } from "react-redux";
+import { Login, SignUp } from "./modules";
+import Header from "./modules/header/headerComponent";
+import { history } from "./managers/history";
 import BaseComponent from "./modules/baseComponent";
-import AllProposal from './modules/Dashboard/viewProposal';
+import AllProposal from "./modules/Dashboard/viewProposal";
+import Community from "./modules/CommunityTools";
 class Routes extends BaseComponent {
+  componentDidMount() {}
 
-    componentDidMount() {
-
-    }
-
-    render() {
-        return (
-
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-                <Router history={history}>
-                    <Switch>
-                        <Route exact path={'/'} component={Header}/>
-                        <Route exact path={'/view-all-proposals'} component={AllProposal}/>
-                        <Route exact path={'/sign-up'} component={SignUp}/>
-                        <Redirect exact from='*' to="/"/>
-                    </Switch>
-                </Router>
-            </MuiThemeProvider>);
-    }
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path={"/"} component={Header} />
+            <Route exact path={"/view-all-proposals"} component={AllProposal} />
+            <Route exact path={"/sign-up"} component={SignUp} />
+            <Route exact path={"/community"} component={Community} />
+            <Redirect exact from="*" to="/" />
+          </Switch>
+        </Router>
+      </MuiThemeProvider>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {user: state.user}
+  return { user: state.user };
 };
 export default connect(mapStateToProps)(Routes);
