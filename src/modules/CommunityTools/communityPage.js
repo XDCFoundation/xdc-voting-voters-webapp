205c03bd5e484906ca62043bc924550304cc10a0
deepali <deepali@leewayhertz.com>
2021-09-01 12:44:48 +0530

    Merge branch 'development' of https://bitbucket.org/leewayhertz/xinfin-voting-voters-webapp into xvd-99

---
+++ src/modules/CommunityTools/communityPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import { Column, Row } from "simple-flexbox";
+import "../../assets/styles/custom.css";
+import HeaderMain from "../header/header";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+
+export default function CommunityPage() {
+  return <div>hello</div>;
+}
