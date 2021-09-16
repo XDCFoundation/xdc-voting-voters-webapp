24c28669d1bcea50521be7e29e750cd268542997
deepali <deepali@leewayhertz.com>
2021-09-16 16:59:56 +0530

    commit

---
+++ build/precache-manifest.43695046a8bdce5226d038cb0ac94a76.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f25f4046d8bd275228f7915c558c198",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4871d6123ad342d77d1b",
+    "url": "/static/css/2.de1b995d.chunk.css"
+  },
+  {
+    "revision": "9933de8d2698f4dcebdf",
+    "url": "/static/css/main.3511816a.chunk.css"
+  },
+  {
+    "revision": "4871d6123ad342d77d1b",
+    "url": "/static/js/2.801180b7.chunk.js"
+  },
+  {
+    "revision": "9933de8d2698f4dcebdf",
+    "url": "/static/js/main.fa077fbf.chunk.js"
+  },
+  {
+    "revision": "c66709f3e2e44288d46d",
+    "url": "/static/js/runtime~main.da4cbb6d.js"
+  },
+  {
+    "revision": "0d24a8d4222b30b1d284e1b3e483323b",
+    "url": "/static/media/Attachment.0d24a8d4.svg"
+  },
+  {
+    "revision": "7d8743a46e6def476236e1cde524e417",
+    "url": "/static/media/Community-Tools.7d8743a4.svg"
+  },
+  {
+    "revision": "acc9d2b7b0c07581a8b5d28a5bcd931d",
+    "url": "/static/media/External-Link.acc9d2b7.svg"
+  },
+  {
+    "revision": "24702179c5a1b7116a9aed78f7a17f62",
+    "url": "/static/media/ForwardLogo.24702179.svg"
+  },
+  {
+    "revision": "14a1f5ad675b551880bd60ef0d836d30",
+    "url": "/static/media/Group 30.14a1f5ad.svg"
+  },
+  {
+    "revision": "930e9ca2f64a833ea936aeb23ac1fe27",
+    "url": "/static/media/Group 32.930e9ca2.svg"
+  },
+  {
+    "revision": "5eb1f37f69c594da742beab6dcdb5e1e",
+    "url": "/static/media/Time-Active.5eb1f37f.svg"
+  },
+  {
+    "revision": "c942fc1d8019993ff985c714ac75b9d7",
+    "url": "/static/media/Time-Inactive.c942fc1d.svg"
+  },
+  {
+    "revision": "2c5e7ac4ea27b8ca8a1160d78d962bac",
+    "url": "/static/media/XDC-Logo-Blue.2c5e7ac4.svg"
+  },
+  {
+    "revision": "58e7967239fae9801f0eb78283126629",
+    "url": "/static/media/xdc_logo.58e79672.svg"
+  }
+]);
