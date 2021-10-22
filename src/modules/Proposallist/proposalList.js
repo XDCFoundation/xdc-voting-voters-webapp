cb85651cfde8ee4b73b0f63eb63e5f3b12af737b
rohit <rohitku@leewayhertz.com>
2021-10-22 14:37:24 +0530

    fixed status and date in proposal list

--- src/modules/Proposallist/proposalList.js
+++ src/modules/Proposallist/proposalList.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
         outline: "0px",
         fontSize: "13px",
         color: "#2149b9",
-        fontWeight: "600",
+        fontWeight: "400",
         textAlignLast: "right",
         marginLeft: "7px",
     },
@@ -33,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
         outline: "0px",
         height: "31px",
         fontFamily: "Inter",
-        fontWeight: "600",
+        fontWeight: "400",
         marginLeft: "8px",
         color: "#2A2A2A",
     },
@@ -125,14 +125,14 @@ const useStyles = makeStyles((theme) => ({
         outline: "0px",
         height: "30px",
         fontFamily: "Inter",
-        fontWeight: "600",
+        fontWeight: "400",
     },
     stylePage: {
         border: "0px",
         outline: "0px",
         fontSize: "13px",
         // color: "#2A2A2A",
-        fontWeight: "600",
+        fontWeight: "400",
         color: "#2149B9",
     },
 }));
@@ -354,7 +354,7 @@ const DateSpan = styled.span`
   color: #2a2a2a;
   font-size: 13px;
   font-family: "Inter";
-  font-weight: 600;
+  font-weight: 400;
   margin-left: 8px;
 `;
 const DatePickerDiv = styled.div`
@@ -376,7 +376,7 @@ const Input = styled.input`
   font-size: 12px;
   font-family: "Inter";
   color: #909090;
-  font-weight: 600;
+  font-weight: 400;
 `;
 const MainContainer = styled.div`
   padding: 20px 17px;
@@ -438,7 +438,7 @@ const PagingDiv = styled.div`
 const Span = styled.span`
   text-align: left;
   font-family: "Inter", sans-serif;
-  font-weight: 600;
+  font-weight: 400;
   font-size: 12px;
   letter-spacing: 0px;
   color: #3b3b3b;
@@ -451,7 +451,7 @@ const Show = styled.span`
   display: flex;
   text-align: left;
   font-family: "Inter", sans-serif;
-  font-weight: 600;
+  font-weight: 400;
   font-size: 12px;
   letter-spacing: 0px;
   color: #3b3b3b;
@@ -467,7 +467,7 @@ const Block = styled.div`
   height: 22px;
   margin-left: 8px;
   font-family: "Inter", sans-serif;
-  font-weight: 600;
+  font-weight: 400;
   font-size: 12px;
 `;
 
