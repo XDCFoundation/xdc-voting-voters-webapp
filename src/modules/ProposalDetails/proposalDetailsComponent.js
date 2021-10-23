1d604ec3acecfe7c474e78a2dd6b1814aea84975
sushmita_SushmitaRout <sushmita@leewayhertz.com>
2021-10-23 12:44:25 +0530

    bug fixing

--- src/modules/ProposalDetails/proposalDetailsComponent.js
+++ src/modules/ProposalDetails/proposalDetailsComponent.js
@@ -251,13 +251,7 @@ export default function ProposalDetails(props) {
                                 <div className="display-flex p-polling-contract">
                                     <div className="polling-heading">Polling Contract</div>
                                     <div className="address-number">
-                                        {props.state.proposalAddress}
-                                        <span>
-                      <img
-                          className="external-image-proposal4"
-                          src={require("../../assets/styles/images/External-Link.svg")}
-                      ></img>
-                    </span>
+                                        {proposalAddressComponent(props.state.proposalAddress)}
                                     </div>
                                 </div>
                             </div>
@@ -319,6 +313,25 @@ function AddressComponent(row) {
                 <div>
                     {Utils.epocToPrettyTime(row.createdOn)}
                 </div>
+            </Row>
+        </>
+    );
+}
+
+
+function proposalAddressComponent(row) {
+    return (
+        <>
+            <Row className="p-8 justify-content-between">
+                <Row>
+                    <div className="fs-15 color-2A2A2A p-l-sm">
+                        {row.substr(0, 13)}...{row.substr(row.length - 5, 5)}
+                    </div>
+                </Row>
+                <img
+                          className="external-image-proposal4"
+                          src={require("../../assets/styles/images/External-Link.svg")}
+                      ></img>
             </Row>
         </>
     );
