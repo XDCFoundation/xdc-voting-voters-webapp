dd6730c67a4c9e9048553fe991ff619bfd1a32ac
nadeem590 <nadeem@leewayhertz.com>
2022-02-24 18:56:24 +0530

    bugs done

--- src/modules/Proposallist/index.js
+++ src/modules/Proposallist/index.js
@@ -9,6 +9,7 @@ import {history} from "../../managers/history";
 import moment from 'moment'
 import Web3 from "web3";
 import {abi as masterContractAbi} from "../../common/abis/masterContractAbi.json";
+import {getTotalVotingAddress} from "../../services/proposalService";
 
 export default class Listui extends BaseComponent {
 
@@ -36,10 +37,32 @@ export default class Listui extends BaseComponent {
     }
 
     parseProposalList = async (proposals)=>{
+        // for(let index=0; index< proposals.length; index++){
+        //     proposals[index].title = await this.getProposalTitle(proposals[index].pollingContract)
+        // }
+        // console.log(proposals,"proposals");
+        // return proposals;
+        let web3;
+        web3 = new Web3(window.web3.currentProvider);
+        window.ethereum.enable();
+        const accounts = await web3.eth.getAccounts();
+        if(!accounts || !accounts.length)
+            return proposals;
+        const addresses = await getTotalVotingAddress();
+        let isWhiteListed = false;
+        for(let index=0; index< addresses.dataList.length; index++){
+            if(addresses.dataList[index].address.toLowerCase() === accounts[0].toLowerCase())
+                isWhiteListed = true;
+        }
+        console.log("isWhiteListed ",isWhiteListed, accounts[0])
+        if(!isWhiteListed)
+            return proposals;
         for(let index=0; index< proposals.length; index++){
             proposals[index].title = await this.getProposalTitle(proposals[index].pollingContract)
         }
+
         return proposals;
+        
     }
 
     getProposalTitle = async (proposalAddress)=>{
@@ -65,6 +88,8 @@ export default class Listui extends BaseComponent {
         const reqObj = {"skip": 0, "limit": this.state.limit, proposalTitle: e.target.value}
         let [error, proposals] = await Utils.parseResponse(proposalList(reqObj));
         console.log("=== ", proposals)
+        // const updatedList = await this.parseProposalList(proposals?.proposalList)
+        // this.setState({proposalsList: updatedList, totalProposalsCount: proposals.countData})
         this.setState({proposalsList: proposals.proposalList})
     };
 
@@ -107,6 +132,7 @@ export default class Listui extends BaseComponent {
                     proposalRedirect={this.proposalRedirect}
                     onStatusChange={this.onStatusChange}
                     onDateChange={this.onDateChange}
+                    
                 />
             </div>
         );
