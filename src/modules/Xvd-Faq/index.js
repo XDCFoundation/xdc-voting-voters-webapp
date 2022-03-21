2495e5f4f9d8d6aa3cea07f48933b7fa06f906b5
Nitin sharma <nitinsharma75604@gmail.com>
2022-03-21 15:25:42 +0530

    bugs fixed,.....,

--- src/modules/Xvd-Faq/index.js
+++ src/modules/Xvd-Faq/index.js
@@ -1,33 +1,13 @@
-// import React from "react";
-// import BaseComponent from "../baseComponent";
-// import FaqComponent from "./Faq";
-// // import FooterComponent from "../footer/footerComponent";
-// // import HeaderMain from "../header/header";
-// // import Sidebar from "../dashboard/sidebar";
+import React from "react";
+import BaseCompoment from "../baseComponent";
+import Xvdfaq from "./xvdFaq";
 
-// import { Row } from "simple-flexbox";
-
-
-// class FAQ extends BaseComponent {
-//   constructor(props) {
-//     super(props);
-//     this.state = {};
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//   {/* <HeaderMain/> */}
-//         <Row>
-//           {/* {window.innerWidth >= 1024 ? <Sidebar /> : ""} */}
-//           <FaqComponent />
-         
-//         </Row>
-//         {/* <FooterComponent/> */}
-        
-//       </div>
-//     );
-//   }
-// }
-
-// export default FAQ;
+export default class Faq extends BaseCompoment {
+  render() {
+    return (
+      <div>
+        <Xvdfaq />
+      </div>
+    );
+  }
+}
