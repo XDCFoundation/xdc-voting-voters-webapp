31526b111f60adbd954e410a6e7138243649b6d8
vishal8288826277 <vishals@leewayhertz.com>
2022-05-05 16:02:32 +0530

    UIupdate

--- src/modules/AddressGroup/addressgroup.js
+++ src/modules/AddressGroup/addressgroup.js
@@ -18,15 +18,15 @@ const AddressGroupTabs = styled.div`
   align-items: center;
   border: 1px solid #e3e7eb;
   font-size: 20px;
-//   font-weight: bold;
+  //   font-weight: bold;
   letter-spacing: 0px;
   color: black;
 `;
 
 const Groupbtn = styled.div`
-margin-left: 741px;
+  margin-left: 741px;
   margin-top: 13px;
-//   width: 123px;
+  //   width: 123px;
   height: 32px;
   border: 1px solid #2049b9;
   border-radius: 4px;
@@ -39,8 +39,8 @@ margin-left: 741px;
 // `;
 
 const Addressbtn = styled.div`
-   margin-top: 13px;
-//   width: 123px;
+  margin-top: 13px;
+  //   width: 123px;
   height: 32px;
   border: 1px solid #2049b9;
   border-radius: 4px;
@@ -54,9 +54,12 @@ const Management = styled.div``;
 
 const Employees = styled.div``;
 
+const Row = styled.div``;
+
 const AddrContainer = styled.div`
   display: flex;
 `;
+
 const GrpContainer = styled.div`
   width: 865px;
   padding: 0px 40px;
@@ -66,8 +69,7 @@ const Addrgrp = styled.div`
   width: 322px;
 `;
 
-const AddressContainer = styled.div
-`
+const AddressContainer = styled.div`
   margin-top: 13px;
   width: 1280px;
   height: 728px;
@@ -76,8 +78,7 @@ const AddressContainer = styled.div
   opacity: 1;
   margin-left: 300px;
 `;
-const GroupContainer = styled.div
-`
+const GroupContainer = styled.div`
   height: 69px;
   border: 1px solid #e3e7eb;
   background-color: white;
@@ -94,8 +95,7 @@ const AddressItem = styled.div`
   align-items: center;
 `;
 
-const Back = styled.div
-`
+const Back = styled.div`
   margin-left: 333px;
   margin-top: -23px;
   font-family: "Inter", sans-serif;
@@ -104,40 +104,103 @@ const Back = styled.div
 `;
 
 const addresses = [
-     {image:"/images/nft_pic.png", address:"xdc517f51dea1ab6468ca2192433a58cbeff0482a0b"},
-     {image:"/images/nft_pic.png", address:"xdc517f51dea1ab6468ca2192433a58cbeff0482a0b"},
-     {image:"/images/nft_pic.png", address:"xdc517f51dea1ab6468ca2192433a58cbeff0482a0b"},
-     {image:"/images/nft_pic.png", address:"xdc517f51dea1ab6468ca2192433a58cbeff0482a0b"}
-  ];
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Top Decision Makers",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Top Decision Makers",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Top Decision Makers",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Top Decsion Makers",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Management",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Management",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Management",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Management",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Employees",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Employees",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Employees",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6468ca2192433a58cbeff0482a0b",
+    groupType: "Employees",
+  },
+  {
+    image: "/images/nft_pic.png",
+    address: "xdc517f51dea1ab6123456789433a58cbeff0482a0b",
+    groupType: "Employees",
+  },
+];
 
-  const AddressGroup = () => {
+const groupList = [
+  {
+    groupName: "Top Decision Makers",
+  },
+  {
+    groupName: "Management",
+  },
+  {
+    groupName: "Employees",
+  },
+];
+
+const AddressGroup = () => {
   const [showAddress, setshowAddress] = useState(false);
   const [showpopOver, setshowpopOver] = useState(false);
   const [showGroup, setshowGroup] = useState(false);
   const [showAddAddr, setshowAddAddr] = useState(false);
-  
+  const [addressNamelist, setaddressNamelist] = React.useState(groupList);
+  const [addGroupInput, setAddGroupInput] = useState("New Group");
+  const [selectedGreoupAddress, setSeletedGroupAddresses] = useState("Top Decision Makers");
+
+  const addgrouphandler = () => {
+    setaddressNamelist([...addressNamelist, { groupName: addGroupInput }]);
+  };
+
+  console.log(selectedGreoupAddress);
+
   const backButton = () => {
-        history.push("/");
-    };
-
-//   const handleKebabIcon = (event) => {
-//         this.setState({
-//             Topics: event.target.value
-//         })
-//     }
-//     const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
-
-//     const handleClick = (event) => {
-//        setAnchorEl(event.currentTarget);
-//    };
-
-//   const handleClose = () => {
-//     setAnchorEl(null);
-//    };
-
-//   const open = Boolean(anchorEl);
-//   const id = open ? 'simple-popover' : undefined;
-    
+    history.push("/");
+  };
 
   return (
     <div>
@@ -147,52 +210,61 @@ const addresses = [
           <img
             onClick={backButton}
             src="/images/Back-Arrow.svg"
-            style={{ marginLeft: "300px", marginTop: "133px",}}
+            style={{ marginLeft: "300px", marginTop: "133px" }}
           />
           <Back>Back</Back>
         </div>
-        
-      
 
-      <AddressContainer>
-        <GroupContainer className="flex justify-between">
-          <addrHead className="addressgrp">Address Group</addrHead>
-          <Groupbtn>
-            <button onClick={() => setshowGroup(true)}>New Group</button>
-          </Groupbtn>
-          <Addressbtn>
-            <button onClick={() => setshowAddAddr(true)}>Add Address</button>
-          </Addressbtn>
-        </GroupContainer>
+        <AddressContainer>
+          <GroupContainer className="flex justify-between">
+            <addrHead className="addressgrp">Address Group</addrHead>
+            <Groupbtn>
+              <button onClick={addgrouphandler}>New Group</button>
+            </Groupbtn>
+            <Addressbtn>
+              <button onClick={() => setshowAddAddr(true)}>Add Address</button>
+            </Addressbtn>
+          </GroupContainer>
 
-        <AddrContainer>
-          <Addrgrp style={{position: "relative"}}>
-            <AddressGroupTabs onClick={() => setshowAddress(true)}>
-              Top Decision Makers
-              <img className="kebabIcon" src = "/images/nft_pic.png" onClick={() => setshowpopOver(true)}></img>
-            </AddressGroupTabs>
-            { showpopOver && (
-            <div className="popOver"> 
-                  <p className="popoverItems" onClick={() => (true)}>Rename</p>
-                  <p className="popoverItems">Delete</p>
-              </div>
-            )}
-            <AddressGroupTabs>Management</AddressGroupTabs>
-            <AddressGroupTabs>Employees</AddressGroupTabs>
-          </Addrgrp>
-          {showAddress && (
-            <GrpContainer>
-              {addresses.map((item) => (
-                <AddressItem ><img src={item.image}/>{item.address}</AddressItem>
+          <AddrContainer>
+            <Addrgrp style={{ position: "relative" }}>
+              {addressNamelist.map((item) => (
+                <AddressGroupTabs
+                  onClick={() => setSeletedGroupAddresses(item.groupName)}
+                >
+                  {item.groupName}
+                  {/* <img className="kebabIcon" src = "/images/nft_pic.png" onClick={() => setshowpopOver(true)}></img> */}
+                </AddressGroupTabs>
               ))}
-            </GrpContainer>
-          )}
-        </AddrContainer>
-        <div></div>  
-      </AddressContainer>
-      </div>
+              {showpopOver && (
+                <div className="popOver">
+                  <p className="popoverItems" onClick={() => true}>
+                    Rename
+                  </p>
+                  <p className="popoverItems">Delete</p>
+                </div>
+              )}
+              {/* <AddressGroupTabs>Management</AddressGroupTabs>
+              <AddressGroupTabs>Employees</AddressGroupTabs> */}
+            </Addrgrp>
 
-      
+            <GrpContainer>
+              {addresses
+                .filter((item) => item.groupType === selectedGreoupAddress)
+                .map((item) => (
+                  <AddressItem>
+                    <img src={item.image} />
+                    {item.address}
+                  </AddressItem>
+                ))}
+            </GrpContainer>
+          </AddrContainer>
+
+          <div></div>
+        </AddressContainer>
+      </div>
+    
+
       <div className="footer-all">
         <FooterComponent />
       </div>
